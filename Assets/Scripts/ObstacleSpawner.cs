b730be824786241e347afed3714938a8aa69068a
Joshua Patterson <jleegippies@gmail.com>
2019-10-26 17:07:13 -0500

    Added Game Over screen

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -33,7 +33,7 @@ public class ObstacleSpawner : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-5.5f, 5.5f), 7.0f, 0);
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-5.5f, 5.5f), 7.0f, 0.0f);
         Vector3 randomRotation = Vector3.forward * Random.Range(0f, 360f);
         float randomSize = Random.Range(0.25f, 3.0f);
 
---
+++ Assets/Scripts/UIBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBehavior : MonoBehaviour
+{
+    PlayerBehavior player;
+    bool isGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        player = FindObjectOfType<PlayerBehavior>();
+        isGameOver = false;
+        player.OnPlayerDeath += GameOver;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        gameObject.SetActive(true);
+    }
+}
