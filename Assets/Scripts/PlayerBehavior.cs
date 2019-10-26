2cf4e384955f0f901d0b53ea57eeb23f0ac4eac4
Joshua Patterson <jleegippies@gmail.com>
2019-10-26 15:03:12 -0500

    Added player movement

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    const float speed = 6f;
+
+    Vector3 input;
+    Rigidbody playerRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+        playerRigidbody.MovePosition(transform.position + input.normalized * speed * Time.fixedDeltaTime);
     }
 }
